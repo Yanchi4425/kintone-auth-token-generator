6c10629159ffa8f4cab267a70455eb9ad50c634c
Yanchi4425 <mail@yama-taku.com>
2019-10-04 18:09:05 +0900

    fix github pages assets.

---
+++ docs/precache-manifest.a3fc87f7ab0e06fa592d4c77ec0192f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56fe26e004422761a46d77375283c395",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccf522b6fd649b762333",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "df04022f8fba18b4d15c",
+    "url": "/static/js/2.72f33387.chunk.js"
+  },
+  {
+    "revision": "ccf522b6fd649b762333",
+    "url": "/static/js/main.abf82e29.chunk.js"
+  },
+  {
+    "revision": "c48d4d476d21915f8d6e",
+    "url": "/static/js/runtime-main.c0ee49b6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
