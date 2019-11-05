d0db5ff1c532d0ae5a866bd42deb7a7812111021
Yanchi4425 <mail@yama-taku.com>
2019-11-05 14:25:33 +0900

    add asset

---
+++ docs/precache-manifest.86b98aa4914f91c23070e0bb8c425f58.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6811552096be462d9ad0da6d220376c5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c87eddb2b329b244d819",
+    "url": "./static/js/2.f77e0ba1.chunk.js"
+  },
+  {
+    "revision": "27a8c99919b8710f5807",
+    "url": "./static/js/main.d1c7c962.chunk.js"
+  },
+  {
+    "revision": "edc3b79f574ceafe5ed5",
+    "url": "./static/js/runtime-main.f34e9fe6.js"
+  }
+]);
